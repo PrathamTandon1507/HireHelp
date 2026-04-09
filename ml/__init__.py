76ec8d98e5152f285e945f6399f565b376bd2090
PrathamTandon1507 <prattandon1507@gmail.com>
2026-04-09 23:27:35 +0530

    changed rag setup to reduce overhead

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,14 +1,12 @@
-"""ML module - RAG system for HireHelp"""
+"""ML module - RAG system for HireHelp (BM25 + Groq)"""
 
 from .models import JobModel, CandidateProfile, RAGOutput
-from .vector_store import FAISSVectorStore
 from .rag_system import RAGAnalyzer, initialize_rag, get_rag_analyzer, analyze_application
 
 __all__ = [
     "JobModel",
     "CandidateProfile",
     "RAGOutput",
-    "FAISSVectorStore",
     "RAGAnalyzer",
     "initialize_rag",
     "get_rag_analyzer",
